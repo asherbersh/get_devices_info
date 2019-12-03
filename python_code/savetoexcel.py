d50978d3cc384d9d73abdb739457be0428bff821
Asher Bershtein <Asher.Bershtein@cellebrite.com>
2019-12-03 12:12:10 +0200

    Save dictonary data that collected from switches to excel

--- python_code/savetoexcel.py
+++ python_code/savetoexcel.py
@@ -15,23 +15,22 @@ def data_to_excel(col,data,save_fn,newline=True):
     wb.save(filename=save_fn)
     return "data was saved"
 
-def dict_to_excel(dict_data,save_fn,newline=True):
+def dict_to_excel(dict_data, save_fn, newline=True):
     # get data to enter to exisiting excel file and insert it in the next free raw
-    # dict_data - data to insert to excel that include column and data to be insert 
-    # save_fn - file to be saved 
-    # newline - to insert the data in a new line or in the last line 
-    wb=load_workbook(filename=save_fn)
-    sheet=wb.active
-
+    # dict_data - data to insert to excel that include column and data to be insert
+    # save_fn - file to be saved
+    # newline - to insert the data in a new line or in the last line
+    wb = load_workbook(filename=save_fn)
+    sheet = wb.active
 
     if newline == True:
-        insert_raw=sheet.max_row+1
-        for col,data in dict_data.items():
-            sheet. cell(insert_raw,col,data)
+        insert_raw = sheet.max_row+1
+        for col, data in dict_data.items():
+            sheet. cell(insert_raw, col, data)
     else:
-        insert_raw=sheet.max_row
-        for col,data in dict_data.items():
-          sheet.cell(insert_raw,col,data)  
+        insert_raw = sheet.max_row
+        for col, data in dict_data.items():
+            sheet.cell(insert_raw, col, data)
     wb.save(filename=save_fn)
     return "data was saved"
 
@@ -42,5 +41,6 @@ def dict_to_excel(dict_data,save_fn,newline=True):
 #print (data_to_excel(2,"192.168.0.3","device_report.xlsx",False))
 #print (data_to_excel(3,"15.0","device_report.xlsx",False))
 
-dict={1:"asher1",2:"the men1"}
-print (dict_to_excel(dict,"device_report.xlsx",False))
+dict1={1:"asher1",2:"the men1"}
+dict2={1:"host_name",2:"switch_ver",3:"product_model",4:"ip_address",5:"vc_count",7:"re_cpu",8:"re_memory"}
+print (dict_to_excel(dict2,"device_report.xlsx"))
